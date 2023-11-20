c274e2e343c1999407438c61a1cc3c6e71504bc7
shashank <shashankprince523@gmail.com>
2023-11-20 19:28:58 +0530

    basic UI 2

--- app/src/main/java/com/shashanksp/smartsonic/CreateAccountActivity.java
+++ app/src/main/java/com/shashanksp/smartsonic/CreateAccountActivity.java
@@ -6,18 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CreateAccountActivity extends AppCompatActivity {
     Button registerbtn;
     TextView signinbtn;
+    EditText guideIDEdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         registerbtn = findViewById(R.id.register_button);
         signinbtn = findViewById(R.id.Signin_txtbtn);
-
+        guideIDEdt = findViewById(R.id.GuideID_edt);
         registerbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +39,29 @@ public class CreateAccountActivity extends AppCompatActivity {
             }
         });
 
+
+        final RadioGroup radio = (RadioGroup) findViewById(R.id.radiogrp);
+        radio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                View radioButton = radio.findViewById(checkedId);
+                int index = radio.indexOfChild(radioButton);
+
+
+                switch (index) {
+                    case 0: // first button
+                        guideIDEdt.setVisibility(View.VISIBLE);
+                        Toast.makeText(getApplicationContext(), "Selected button number " + index, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1: // secondbutton
+                        guideIDEdt.setVisibility(View.GONE);
+                        Toast.makeText(getApplicationContext(), "Selected button number " + index, Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
 
     }
 }
--- app/src/main/java/com/shashanksp/smartsonic/SignInActivity.java
+++ app/src/main/java/com/shashanksp/smartsonic/SignInActivity.java
@@ -6,18 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
     Button loginbtn;
     TextView signupbtn;
+    EditText guideIDEdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         loginbtn = findViewById(R.id.login_button);
         signupbtn = findViewById(R.id.createacc_txtbtn);
-
+        guideIDEdt = findViewById(R.id.GuideID_edt);
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +35,29 @@ public class SignInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(SignInActivity.this,CreateAccountActivity.class);
                 startActivity(i);
+            }
+        });
+
+        final RadioGroup radio = (RadioGroup) findViewById(R.id.radiogrp);
+        radio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                View radioButton = radio.findViewById(checkedId);
+                int index = radio.indexOfChild(radioButton);
+
+
+                switch (index) {
+                    case 0: // first button
+                        guideIDEdt.setVisibility(View.VISIBLE);
+                        Toast.makeText(getApplicationContext(), "Selected button number " + index, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1: // secondbutton
+                        guideIDEdt.setVisibility(View.GONE);
+                        Toast.makeText(getApplicationContext(), "Selected button number " + index, Toast.LENGTH_SHORT).show();
+                        break;
+                }
             }
         });
 
