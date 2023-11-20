a3a7cbdd4a68c6e097e033a427847971a214e412
shashank <shashankprince523@gmail.com>
2023-11-20 20:09:22 +0530

    basic UI 4

--- app/src/main/java/com/shashanksp/smartsonic/PaymentGatewayActivity.java
+++ app/src/main/java/com/shashanksp/smartsonic/PaymentGatewayActivity.java
@@ -2,13 +2,25 @@ package com.shashanksp.smartsonic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PaymentGatewayActivity extends AppCompatActivity {
-
+    Button payBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_gateway);
+        payBtn = findViewById(R.id.payBtn);
+
+        payBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PaymentGatewayActivity.this,ListenActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
